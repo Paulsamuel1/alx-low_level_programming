4578d8dff993066727ae1add4d593de70153143a
Paulsamuel <chigozie79@gmail.com>
2023-02-22 17:49:07 -0800

    Alphbet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *  print_alphabet_x10 - function that prints 10x alphabets
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- *  Return: always 0
+ * Return: Always 0 (Success)
  */
 
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	char j;
 
-	for (i = 1 ; i <= 'z' ; j++)
+	for (i = 1 ; i <= 10 ; i++)
 	{
 		for (j = 'a' ; j <= 'z' ; j++)
 			_putchar(j);
