8e0268be1c999468d8eeffd428228bcf2e812428
Paulsamuel1 <chigozie79@gmail.com>
2023-05-08 21:00:36 -0700

    Created a File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
+ * create_file - program creates a file
  * @filename: filename.
  * @text_content: content writed in the file.
  *
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int vv;
+	int fd;
 	int nletters;
 	int rwr;
 
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (vv == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 	if (rwr == -1)
 		return (-1);
 
-	close(vv);
+	close(fd);
 
 	return (1);
 }
